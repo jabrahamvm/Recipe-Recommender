8276116e26ffef44aef0accce7c6b9086fb20c27
Astralpuppy <rodrigonev98@gmail.com>
2022-11-20 09:59:14 -0700

    Preparando datos para API

---
+++ api/main.py
@@ -0,0 +1,10 @@
+import json
+import pandas as pd 
+import numpy as np 
+import nltk  as nltk
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import flask
+
+def recommender(ingredientes)
+    
